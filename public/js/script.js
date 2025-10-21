041ad1f06aa04ced8f26eaec53a9f505dfeda228
Elvin Hrytsyuk <183242754+Elvin-code-dev@users.noreply.github.com>
2025-10-21 12:08:25 -0700

    changes to filies

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-// Global variables
-const formRef = document.getElementById("schedular-form");
-const inputFields = {
-	fname: document.getElementById("fname"),
-	lname: document.getElementById("lname"),
-	date: document.getElementById("date"),
-	time: document.getElementById("time"),
-};
-const submitBtn = document.getElementById("submit-btn");
